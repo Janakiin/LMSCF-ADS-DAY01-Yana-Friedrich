8205f7b558325074bf4f925e3fae55e7bdb1bf4c
Fritz Pick <fritzpick@posteo.at>
2020-01-22 13:52:53 +0100

    add code in seconds programm

--- seconds/seconds.js
+++ seconds/seconds.js
@@ -1,1 +1,18 @@
 var seconds = prompt("Enter a number");
+var seconsInMinute = 60;
+var secondsInHour = 3600;
+var secondsInDay = 86400;
+var secondsInWeek = 604800;
+var numberOf;
+var secondsInNumberOf;
+var remainingSeconds;
+
+function count(a,b) {
+	numberOf = a / b;
+	secondsInNumberOf = Math.floor(numberOf) * b;
+	remainingSeconds = a - secondsInNumberOf;
+	a 
+}
+
+
+
